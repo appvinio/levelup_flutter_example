e726e52b1f6f3edc3d8a420409c68e1baa3661e9
rafal.dziuryk <rafal.dziuryk@gmail.com>
2018-07-19 15:42:38 +0200

    add title as textfield

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
+        title: TextField(),
       ),
       body: new Center(
       ),
